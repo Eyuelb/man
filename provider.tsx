5425f4413b11f0ce743655cb777b80fa4a4f1d73
Eyuel Belete <eyuelbelete98@gmail.com>
2023-10-07 00:33:34 +0300

    update

--- provider.tsx
+++ provider.tsx
@@ -6,7 +6,7 @@ import { theme as baseTheme } from './theme';
 
 import './globals.css';
 import '@mantine/core/styles.css';
-import '@mantine/notifications/styles.css';
+import '@mantine/notifications/styles.css'; 
 
 export default function RootStyleRegistry({
   children,
